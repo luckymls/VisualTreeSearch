f13cd8f01399cd85cb6e2c094c69bc7b72d08d88
idduperezza <pierangeloloi@hotmail.it>
2022-05-15 16:52:24 +0200

    Forse ho fixato il bidirezionale, controlla pls

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from search_strategies import tree_search, BFS, IDS, BFS_bidirectional, DFS
 
 if __name__ == '__main__':
 
-    initial_state = [1,2,3,4,8,5,7,0,6]
-    goal_state = [1,2,3,4,5,6,7,8,0]
-    
-    
+    initial_state = [1, 2, 3, 4, 8, 5, 7, 0, 6]
+    goal_state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+
     if is_solvable(initial_state):
         
         print("Puzzle is solvable")
@@ -20,7 +19,7 @@ if __name__ == '__main__':
         problem = Problem('8tile', initial_state, goal_state, 1, step_graph) 
 
         choice = input("Algorithm\n[0] IDS\n[1] BFS\n[2] BFS bidirectional\n[3] DFS\n> ")
-        while choice not in ['0','1','2','3']:
+        while choice not in ['0', '1', '2', '3']:
             choice = input("Algorithm\n[0] IDS\n[1] BFS\n[2] BFS bidirectional\n[3] DFS\n> ")
         choice = int(choice)
         if choice == 0:
@@ -31,22 +30,22 @@ if __name__ == '__main__':
             result = tree_search(problem, BFS_bidirectional)
         if choice == 3:
             print("Da implementare.")
-            result=None
-            #result = tree_search(problem, DFS)
+            result = None
+            # result = tree_search(problem, DFS)
 
         if not result:
             print("No solution found")
         else:
 
             # Temporaneo, crea il pdf del vettore dei risultati
-            index=0
+            index = 0
             for node in reversed(result):
                 
-                if node.state != None:
+                if node.state is not None:
                     result_graph.add_node(str(index), node.state)
                     if index > 0:
                         result_graph.add_edge(str(index-1), str(index), node.action)
-                index +=1
+                index += 1
             result_graph.graph_viewer()
             # step_graph.graph_viewer()
             
